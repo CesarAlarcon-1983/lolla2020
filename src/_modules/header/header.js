4d59ce9d54d7bb54886cb3a8bc9e61e818003575
Your Name <you@example.com>
2019-08-07 04:20:22 -0300

    Updates

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -5,17 +5,43 @@ var Header = function() {
     var header = $('.header');
     var body = $('body');
     var menuOpen = $('.header__hamburguer');
-    var menuClose = $('.header__nav__close');
+    var contentWrapper = $('.home__content__wrapper--middle');
+    var mainWrapper = $('.js-wrapper');
+    var closeButton = $('.home__content__close-button');
 
     menuOpen.on('click', function(){
         header.addClass('-open');
         body.addClass('-hideOverflow');
     });
 
-    menuClose.on('click', function(){
-        header.removeClass('-open');
-        body.removeClass('-hideOverflow');
+    var targets = $('[data-target]');
+    var contents = $('[data-content]');
+
+    function init() {
+        mainWrapper.addClass('-active')
+    }
+
+    closeButton.on('click', function() {
+        targets.removeClass('-active');
+        contents.removeClass('-active');
+        mainWrapper.addClass('-active')
+
     });
+
+    if (targets.length > 0) {
+        targets.on('click', function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            var targettedContent = $this.data('target');
+
+            targets.removeClass('-active');
+            contents.removeClass('-active');
+            mainWrapper.removeClass('-active')
+            $('[data-content="'+ targettedContent +'"]').addClass('-active');
+        });
+    }
+    
+    init();
 };
 
 module.exports = Header;
